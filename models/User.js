3c3cbea71e505274b6d2059814085bbddd4ddb28
HeitorSamuel <heitor10samuel@hotmail.com>
2024-06-06 06:34:15 -0300

    modificação atual

--- models/User.js
+++ models/User.js
@@ -1,76 +1,26 @@
-// const Sequelize = require('sequelize');
-// const db = require('./db');
-
-// const User = db.define('alunos', {
-//     matricula: {
-//         type: Sequelize.INTEGER,
-//         allowNull: false,
-//         primaryKey: true
-//     },
-//     nome: {
-//         type: Sequelize.STRING,
-//         allowNull: false,
-//     },
-//     email: {
-//         type: Sequelize.STRING,
-//         allowNull: false,
-//     }
-// }, {
-//     timestamps: false // Desativa as colunas createdAt e updatedAt
-// });
-
-// module.exports = User;
-
-// const { DataTypes } = require('sequelize');
-// const { sequelize } = require('./db');
-
-// const User = sequelize.define('alunos', {
-//     matricula: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         primaryKey: true
-//     },
-//     nome: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     email: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     salapc: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     }
-// }, {
-//     timestamps: false
-// });
-
-// module.exports = User;
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('./db');
-
-const User = sequelize.define('alunos', {
-    matriculas: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true
-    },
-    nome: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    salapc: {
-        type: DataTypes.STRING,
-        allowNull: false
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-}, {
-    timestamps: false
-});
-
-module.exports = User;
-
+  }
+  User.init({
+    matriculas: DataTypes.INTEGER,
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    salapc: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
